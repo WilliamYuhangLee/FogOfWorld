7c477b98d448b68e0adf26f7ffca884a89a8fa94
Yuhang Li <18326007+WilliamYuhangLee@users.noreply.github.com>
2019-10-14 07:20:55 -0400

    Add models (User, Account)

---
+++ server/src/main/java/li/yuhang/fogofworld/server/model/Account.java
@@ -0,0 +1,35 @@
+package li.yuhang.fogofworld.server.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Accessors(chain = true)
+@Table(name = "accounts", indexes = {@Index(columnList = "user_id", unique = true)})
+public class Account {
+
+    @Id
+    private String username;
+
+    @OneToOne
+    @JoinColumn(unique = true, nullable = false)
+    private User user;
+
+    @NonNull
+    @Column(nullable = false)
+    private String password;
+}
---
+++ server/src/main/java/li/yuhang/fogofworld/server/model/User.java
@@ -0,0 +1,31 @@
+package li.yuhang.fogofworld.server.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Accessors(chain = true)
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "user")
+    private Account account;
+}
